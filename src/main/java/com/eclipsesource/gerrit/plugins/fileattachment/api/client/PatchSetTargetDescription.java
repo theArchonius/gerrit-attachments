31705d91f76692aa418b91b05013d8f319e19f41
Florian Zoubek <zoubek@bitandart.at>
2015-01-21 12:28:50 +0100

    adds basic AttachmentTargetDescription implementations for a change, a patch set and a patch

---
+++ src/main/java/com/eclipsesource/gerrit/plugins/fileattachment/api/client/PatchSetTargetDescription.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.eclipsesource.gerrit.plugins.fileattachment.api.client;
+
+import com.eclipsesource.gerrit.plugins.fileattachment.api.AttachmentTargetDescription;
+
+/**
+ * The {@link AttachmentTargetDescription} for a PatchSet.
+ * 
+ * @author Florian Zoubek
+ *
+ */
+public class PatchSetTargetDescription implements AttachmentTargetDescription {
+
+  private ChangeTargetDescription changeTargetDescription;
+
+  /**
+   * the id of the patch set
+   */
+  private int id;
+
+  public PatchSetTargetDescription(
+      ChangeTargetDescription changeTargetDescription, int id) {
+    super();
+    this.changeTargetDescription = changeTargetDescription;
+    this.id = id;
+  }
+
+  /**
+   * @return the patch set id
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * @param id the patch set id to set
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  /**
+   * @return the changeTargetDescription
+   */
+  public ChangeTargetDescription getChangeTargetDescription() {
+    return changeTargetDescription;
+  }
+
+  /**
+   * @param changeTargetDescription the changeTargetDescription to set
+   */
+  public void setChangeTargetDescription(
+      ChangeTargetDescription changeTargetDescription) {
+    this.changeTargetDescription = changeTargetDescription;
+  }
+
+}
