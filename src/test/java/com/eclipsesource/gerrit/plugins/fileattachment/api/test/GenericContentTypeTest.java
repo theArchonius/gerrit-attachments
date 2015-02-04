ce5b325335322fc20d93eaa74ea388f3e3a16e8d
Florian Zoubek <zoubek@bitandart.at>
2015-02-04 15:52:53 +0100

    adds test cases for GenericContentType creation from content type identifier and content type identifier creation

--- src/test/java/com/eclipsesource/gerrit/plugins/fileattachment/api/test/GenericContentTypeTest.java
+++ src/test/java/com/eclipsesource/gerrit/plugins/fileattachment/api/test/GenericContentTypeTest.java
@@ -3,9 +3,16 @@
  */
 package com.eclipsesource.gerrit.plugins.fileattachment.api.test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+
+import com.eclipsesource.gerrit.plugins.fileattachment.api.ContentType;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.impl.GenericContentType;
 
 /**
  * @author Florian Zoubek
@@ -13,26 +20,69 @@ import org.junit.Test;
  */
 public class GenericContentTypeTest {
 
+  private static final Map<String, String[]> VALID_TYPES = initializeTypes();
+
+  private static final String[] VALID_PARAMETER_NAMES = {"charset",
+      "lowercaseparameter", "0123456789"};
+
+  private static final String[] VALID_PARAMETER_VALUES =
+      {"utf-8", "lowercasevalue", "0123456789",
+          "\"§$%&/()=?':;<>|,.-_#+~*{[]}\\^°µ´²³\""};
+
+
+  private static Map<String, String[]> initializeTypes() {
+    Map<String, String[]> types = new HashMap<String, String[]>();
+
+    types.put("text", new String[] {"plain", "xml"});
+    types.put("application", new String[] {"json"});
+
+    // TODO add more content types
+
+    return types;
+  }
+
   /**
    * Test method for
    * {@link com.eclipsesource.gerrit.plugins.fileattachment.api.impl.GenericContentType#GenericContentType(java.lang.String)}
    * .
    */
   @Test
-  public void testGenericContentTypeString() {
-    // TODO
-    fail("Not yet implemented");
-  }
+  public void testGenericContentTypeFromContentTypeString() {
 
-  /**
-   * Test method for
-   * {@link com.eclipsesource.gerrit.plugins.fileattachment.api.impl.GenericContentType#GenericContentType(java.lang.String, boolean)}
-   * .
-   */
-  @Test
-  public void testGenericContentTypeStringBoolean() {
-    // TODO
-    fail("Not yet implemented");
+    for (String baseType : VALID_TYPES.keySet()) {
+
+      String[] subTypes = VALID_TYPES.get(baseType);
+
+      for (String subType : subTypes) {
+
+        for (int pNameIndex = 0; pNameIndex < VALID_PARAMETER_NAMES.length; pNameIndex++) {
+
+          for (int pValueIndex = 0; pValueIndex < VALID_PARAMETER_VALUES.length; pValueIndex++) {
+
+            String contentTypeIdentifier = baseType + "/" + subType;
+
+            Map<String, String> expectedParameters =
+                new HashMap<String, String>();
+            expectedParameters.put(VALID_PARAMETER_NAMES[pNameIndex],
+                VALID_PARAMETER_VALUES[pValueIndex]);
+
+            contentTypeIdentifier +=
+                ";" + VALID_PARAMETER_NAMES[pNameIndex] + "="
+                    + VALID_PARAMETER_VALUES[pValueIndex];
+
+            ContentType contentType =
+                new GenericContentType(contentTypeIdentifier);
+
+
+            assertThat(
+                "Content type parameter map contains unexpected parameters or values",
+                contentType.getParameters(), is(expectedParameters));
+
+          }
+        }
+      }
+    }
+
   }
 
   /**
@@ -42,8 +92,40 @@ public class GenericContentTypeTest {
    */
   @Test
   public void testGetContentTypeIdentifier() {
-    // TODO
-    fail("Not yet implemented");
+    for (String baseType : VALID_TYPES.keySet()) {
+
+      String[] subTypes = VALID_TYPES.get(baseType);
+
+      for (String subType : subTypes) {
+
+        for (int pNameIndex = 0; pNameIndex < VALID_PARAMETER_NAMES.length; pNameIndex++) {
+
+          for (int pValueIndex = 0; pValueIndex < VALID_PARAMETER_VALUES.length; pValueIndex++) {
+
+            String expectedContentTypeIdentifier = baseType + "/" + subType;
+
+            Map<String, String> parameters = new HashMap<String, String>();
+            parameters.put(VALID_PARAMETER_NAMES[pNameIndex],
+                VALID_PARAMETER_VALUES[pValueIndex]);
+
+            expectedContentTypeIdentifier +=
+                "; " + VALID_PARAMETER_NAMES[pNameIndex] + "="
+                    + VALID_PARAMETER_VALUES[pValueIndex];
+
+            ContentType contentType =
+                new GenericContentType(baseType, subType, parameters);
+
+            assertThat("Unexpected content type identifier",
+                contentType.getContentTypeIdentifier(),
+                is(expectedContentTypeIdentifier));
+
+            // TODO add test for basetype and subtype once they are added to the
+            // ContentType interface
+
+          }
+        }
+      }
+    }
   }
 
 }
