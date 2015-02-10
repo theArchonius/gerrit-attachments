6241d8dda2a4dc4ea0aec778d61c496b43b47b7d
Florian Zoubek <zoubek@bitandart.at>
2015-02-10 12:17:10 +0100

    adds very basic implementation of RestFileAttachmentClientFactoryTest

---
+++ src/test/java/com/eclipsesource/gerrit/plugins/fileattachment/api/test/client/RestFileAttachmentClientFactoryTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.eclipsesource.gerrit.plugins.fileattachment.api.test.client;
+
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.eclipsesource.gerrit.plugins.fileattachment.api.client.RestFileAttachmentClientFactory;
+
+
+/**
+ * @author Florian Zoubek
+ *
+ */
+public class RestFileAttachmentClientFactoryTest {
+
+
+
+  @Test
+  public void testCreateFileAttachmentService() {
+    // TODO improve this test as soon there is a
+    // constructor that allows specification of parameters
+    
+    Assert.assertThat(new RestFileAttachmentClientFactory()
+        .createFileAttachmentClientService(), CoreMatchers
+        .instanceOf(RestFileAttachmentClientService.class));
+  }
+
+}
