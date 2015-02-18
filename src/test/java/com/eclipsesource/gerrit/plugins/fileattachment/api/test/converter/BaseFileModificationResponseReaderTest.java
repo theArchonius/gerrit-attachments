d683d497d399aaa6a3c899884f9999ab5fff7b33
Florian Zoubek <zoubek@bitandart.at>
2015-02-18 14:33:11 +0100

    Adds test for BaseFileModificationResponseReader

--- src/test/java/com/eclipsesource/gerrit/plugins/fileattachment/api/test/converter/BaseFileModificationResponseReaderTest.java
+++ src/test/java/com/eclipsesource/gerrit/plugins/fileattachment/api/test/converter/BaseFileModificationResponseReaderTest.java
@@ -3,9 +3,23 @@
  */
 package com.eclipsesource.gerrit.plugins.fileattachment.api.test.converter;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+
+import com.eclipsesource.gerrit.plugins.fileattachment.api.OperationResult;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.OperationResultType;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.entities.FileModificationResponseEntity;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.entities.FileModificationResponseEntity.FileState;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.entities.OperationResultEntity;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.entities.OperationResultEntity.ResultStatus;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.entities.converter.BaseFileModificationResponseReader;
+import com.eclipsesource.gerrit.plugins.fileattachment.api.impl.GenericOperationResult;
 
 /**
  * @author Florian Zoubek
@@ -14,14 +28,76 @@ import org.junit.Test;
 public class BaseFileModificationResponseReaderTest {
 
   /**
+   * A map containing the expected mapping of {@link ResultStatus} values to
+   * {@link OperationResultType} values
+   */
+  private static final Map<ResultStatus, OperationResultType> RESULTSTATUS_TO_OPERATIONRESULTTYPE_MAP =
+      createOperationResultTypeMap();
+
+  private static Map<ResultStatus, OperationResultType> createOperationResultTypeMap() {
+    Map<ResultStatus, OperationResultType> operationResultTypeMap =
+        new HashMap<ResultStatus, OperationResultType>();
+
+    operationResultTypeMap.put(ResultStatus.FAILED, OperationResultType.FAILED);
+    operationResultTypeMap.put(ResultStatus.NOTPERMITTED,
+        OperationResultType.NOTPERMITTED);
+    operationResultTypeMap.put(ResultStatus.SUCCESS,
+        OperationResultType.SUCCESS);
+
+    return Collections.unmodifiableMap(operationResultTypeMap);
+  }
+
+  /**
+   * A map containing messages for specific {@link ResultStatus}
+   */
+  private static final Map<ResultStatus, String> MESSAGES_MAP =
+      createMessagesMap();
+
+
+  private static Map<ResultStatus, String> createMessagesMap() {
+    Map<ResultStatus, String> messages =
+        new HashMap<OperationResultEntity.ResultStatus, String>();
+
+    messages.put(ResultStatus.FAILED, "An error occured");
+    messages.put(ResultStatus.NOTPERMITTED, "Operation is not permitted");
+    messages.put(ResultStatus.SUCCESS, "");
+
+    return Collections.unmodifiableMap(messages);
+  }
+
+
+  /**
    * Test method for
    * {@link com.eclipsesource.gerrit.plugins.fileattachment.api.entities.converter.BaseFileModificationResponseReader#toObject(com.eclipsesource.gerrit.plugins.fileattachment.api.entities.FileModificationResponseEntity, java.lang.Object)}
    * .
    */
   @Test
   public void testToObject() {
-    // TODO
-    fail("Not yet implemented");
-  }
+    BaseFileModificationResponseReader baseFileModificationResponseReader =
+        new BaseFileModificationResponseReader();
 
+    for (ResultStatus resultStatus : ResultStatus.values()) {
+
+      for (FileState newFileState : FileState.values()) {
+
+        OperationResultEntity operationResultEntity =
+            new OperationResultEntity(resultStatus, "");
+        FileModificationResponseEntity jsonEntity =
+            new FileModificationResponseEntity(operationResultEntity,
+                newFileState);
+
+        OperationResult expectedOperationResult =
+            new GenericOperationResult(
+                RESULTSTATUS_TO_OPERATIONRESULTTYPE_MAP.get(resultStatus),
+                MESSAGES_MAP.get(resultStatus));
+
+        OperationResult operationResult =
+            baseFileModificationResponseReader.toObject(jsonEntity, null);
+
+        assertThat(operationResult, is(expectedOperationResult));
+
+      }
+    }
+
+  }
 }
