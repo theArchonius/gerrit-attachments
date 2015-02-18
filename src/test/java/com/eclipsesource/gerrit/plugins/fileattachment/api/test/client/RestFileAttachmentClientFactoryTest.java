ef291ed5ff98dcec096aa595dbb3fe116f5caffa
Florian Zoubek <zoubek@bitandart.at>
2015-02-18 16:13:12 +0100

    removes stub test methods targeting RestFileAttachmentClientFactory
    Reason: There exists no reasonable test cases for
    RestFileAttachmentClientFactory

--- src/test/java/com/eclipsesource/gerrit/plugins/fileattachment/api/test/client/RestFileAttachmentClientFactoryTest.java
+++ src/test/java/com/eclipsesource/gerrit/plugins/fileattachment/api/test/client/RestFileAttachmentClientFactoryTest.java
@@ -4,29 +4,13 @@
 package com.eclipsesource.gerrit.plugins.fileattachment.api.test.client;
 
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
-import org.junit.Test;
-
-import com.eclipsesource.gerrit.plugins.fileattachment.api.client.RestFileAttachmentClientFactory;
-
-
 /**
  * @author Florian Zoubek
  *
  */
 public class RestFileAttachmentClientFactoryTest {
 
-
-
-  @Test
-  public void testCreateFileAttachmentService() {
-    // TODO improve this test as soon there is a
-    // constructor that allows specification of parameters
-    
-    Assert.assertThat(new RestFileAttachmentClientFactory()
-        .createFileAttachmentClientService(), CoreMatchers
-        .instanceOf(RestFileAttachmentClientServiceTest.class));
-  }
+  // There are currently not reasonable test cases for
+  // RestFileAttachmentClientFactory
 
 }
